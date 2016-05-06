8988901caa81185545a52b3b4c095c7022490140
Lars-H <lars.heling@gmx.de>
2016-05-06 19:04:31 +0200

    Backend Updates: - Worked on Parser to parse event to RDF

---
+++ backend/parser/graphlib.py
@@ -0,0 +1,83 @@
+from rdflib import Graph, Literal, BNode, Namespace, RDF, XSD
+
+global graph
+graph = Graph();
+
+global SSN
+global GEO
+global DUL
+
+# SSN Namespace
+SSN = Namespace('https://www.w3.org/2005/Incubator/ssn/ssnx/ssn#')
+
+# Geo Namespace
+GEO = Namespace('http://www.w3.org/2003/01/geo/wgs84_pos#')
+
+# DUL Namespace
+DUL = Namespace('http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#')
+
+
+def parseDataframe(dataframe):
+    initGraph();
+
+    for index, row in dataframe.iterrows():
+        addToGraph(row)
+
+    return graph;
+
+def initGraph():
+    # Bind Namespace to abbreviation
+    graph.bind('ssn', SSN)
+    graph.bind('geo', GEO)
+    graph.bind('dul', DUL)
+    return
+
+def addToGraph(event):
+    observation = BNode();
+
+    oTime = BNode();
+    # Observation
+    graph.add((observation, RDF.type, SSN.Observation))
+    graph.add((oTime, RDF.type, DUL.TimeInterval))
+    graph.add((observation, SSN.observationSamplingTime, oTime))
+
+    # Time
+    t = Literal(event['dropoff_datetime'], datatype=XSD.dateTime)
+    graph.add((oTime, DUL.hasRegionDataValue, t))
+
+    # SensorOutput
+    sensorOutput = BNode();
+
+    graph.add((sensorOutput, RDF.type, SSN.SensorOutput))
+    graph.add((observation, SSN.observationResult, sensorOutput))
+
+    # ObservationValue
+    observationValue = BNode()
+    startLocation = BNode()
+    endLocation = BNode()
+    graph.add((observationValue, RDF.type, SSN.ObservationValue))
+    graph.add((sensorOutput, SSN.hasValue, observationValue))
+
+    # Start and End Location
+    graph.add((observationValue, SSN.hasStartLocation, startLocation))
+    graph.add((observationValue, SSN.hasEndLocation, endLocation))
+    graph.add((startLocation, RDF.type, GEO.location))
+    graph.add((endLocation, RDF.type, GEO.location))
+
+    # Start Location
+    lat = Literal(event['pickup_latitude'], datatype=XSD.float)
+    long = Literal(event['pickup_longitude'], datatype=XSD.float)
+
+    # Adding the start location
+    graph.add((startLocation, GEO.lat, lat))
+    graph.add((startLocation, GEO.long, long))
+
+    # End Location
+    lat = Literal(event['dropoff_latitude'], datatype=XSD.float)
+    long = Literal(event['dropoff_longitude'], datatype=XSD.float)
+
+    # Adding the start location
+    graph.add((endLocation, GEO.lat, lat))
+    graph.add((endLocation, GEO.long, long))
+
+    return
--- backend/parser/main.py
+++ backend/parser/main.py
@@ -1,1 +1,19 @@
-print("I am the parser")
+import pandas as pd
+import graphlib as gl
+import time
+
+
+df = pd.read_csv('trips_shortend.csv')
+#print(df.columns.values)
+
+#graph = gl.parseEvent(df.ix[0]);
+
+start = time.time()
+graph = gl.parseDataframe(df)
+
+end = time.time()
+elapsed = end-start
+perEvent = elapsed / len(df)
+print(perEvent)
+graph.serialize(destination='output.txt', format='turtle')
+

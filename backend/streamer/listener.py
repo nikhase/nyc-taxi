7e93fc2aa9538fe95864917bdddf65c16516058c
Schlendrikovic <julian94Scherner>
2016-05-26 13:45:57 +0200

    Streamer, Listener Updated

--- backend/streamer/listener.py
+++ backend/streamer/listener.py
@@ -10,3 +10,4 @@ sock.bind((UDP_IP, UDP_PORT))
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print "received message:", data
+
--- backend/streamer/sender.py
+++ backend/streamer/sender.py
@@ -2,7 +2,21 @@ import socket
 import pandas as pd
 import time
 
-df = pd.read_csv("trips_shortend.csv", nrows=10);
+df = pd.read_csv("/Users/Schlendrikovic/Documents/Uni/SS 2016/EventProcessing/Daten/oneweekfrom20130107.csv");
+
+# drop unnecessary columns
+df.drop(
+    ['Unnamed: 0', 'hack_license', 'payment_type', 'pfare_amount', 'surcharge', 'tip_amount', 'total_amount',
+     'passenger_count', 'medallion'], axis=1, inplace=1)
+# print(df.head())
+
+# Datetime right Format
+df['pickup_datetime'] = pd.to_datetime(df['pickup_datetime'], format='%Y-%m-%d %H:%M:%S')
+df['dropoff_datetime'] = pd.to_datetime(df['dropoff_datetime'], format='%Y-%m-%d %H:%M:%S')
+
+# sort by pickup_datetime
+df.sort_values("pickup_datetime")
+df.index = df['pickup_datetime']
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
@@ -15,8 +29,15 @@ print "message:",
 sock = socket.socket(socket.AF_INET,  # Internet
                      socket.SOCK_DGRAM)  # UDP
 
-for i in range(0,9):
+for i in range(0,len(df)-1):
+    date = df.ix[i]
+    print(date)
+    date_1 = df.ix[i][0]
+    date_2 = df.ix[i + 1][0]
+    diff = (pd.to_datetime(date_2) - pd.to_datetime(date_1)).seconds
     MESSAGE = str(df.ix[i])
-
     sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-    time.sleep(3)
+    time.sleep(diff)
+
+
+sock.close()

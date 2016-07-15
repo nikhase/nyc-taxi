d1ee8e8686d124518d3a17bcc38fc851ca7831e0
Niklas H <niklas.haas@student.kit.edu>
2016-07-15 23:29:16 +0200

    Modified to relative path and new pkl files for the models

--- Server/application/datasources/bike_historic/treelib.py
+++ Server/application/datasources/bike_historic/treelib.py
@@ -6,8 +6,8 @@ from sklearn.tree import DecisionTreeRegressor
 
 global regtree
 
-regtree = joblib.load('/Users/larshelin/Documents/PycharmProjects/CEP/nyc-taxi/Server/application/datasources/bike_historic/treelib/bike_regtree_depth_10_PY27.pkl')
-
+# regtree = joblib.load('/Users/larshelin/Documents/PycharmProjects/CEP/nyc-taxi/Server/application/datasources/bike_historic/treelib/bike_regtree_depth_10_PY27.pkl')
+regtree = joblib.load('./application/datasources/bike_historic/treelib/bike_regtree_26x_depth_26_mss_2_PY27.pkl')
 def getEstimatedTime(X):
     y_pred = regtree.predict(X)
     return y_pred
--- Server/application/datasources/taxi_historic/treelib.py
+++ Server/application/datasources/taxi_historic/treelib.py
@@ -6,8 +6,8 @@ from sklearn.tree import DecisionTreeRegressor
 
 global regtree
 
-regtree = joblib.load('/Users/larshelin/Documents/PycharmProjects/CEP/nyc-taxi/Server/application/datasources/taxi_historic/treelib/regtree_depth_30_PY27.pkl')
-
+# regtree = joblib.load('/Users/larshelin/Documents/PycharmProjects/CEP/nyc-taxi/Server/application/datasources/taxi_historic/treelib/regtree_depth_30_PY27.pkl')
+regtree = joblib.load('./application/datasources/taxi_historic/treelib/regtree_depth_25_mss_50_rs_10.pkl')
 def getEstimatedTime(X):
     y_pred = regtree.predict(X)
     return y_pred
